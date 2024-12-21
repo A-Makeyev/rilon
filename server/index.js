02d0619de1cd73284c3a795a22e64db349d64444
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2024-12-21 02:39:50 +0200

    updated project structure

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const MONGODB_URI = process.env.MONGODB_URI
 
-
 app.use(cors({
     origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
